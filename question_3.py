213ba8af5606fdcb060af6de2236dff9cf30d174
B Morgan - Desktop <jammin0921@comcast.net>
2020-04-05 18:13:50 -0500

    question 3

---
+++ question_3.py
@@ -0,0 +1,19 @@
+'''
+url: https://projecteuler.net/problem=3
+Largest prime factor
+
+Problem 3
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+'''
+
+def greatest_prime_factors(n):
+    i = 2
+    while i*i < n:
+        while n%i == 0:
+            n = n / i
+        i = i + 1
+    return int(n)
+
+print(greatest_prime_factors(600851475143))
